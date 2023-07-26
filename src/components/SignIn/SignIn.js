36dfcccacee42574015e11575db9dbf6fb03ca65
ivanotch <90089378+ivanotch@users.noreply.github.com>
2023-07-26 22:24:49 +0800

    Update SignIn.js
    added the backend route

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
     
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin', {
+        fetch('https://facerecogbackend.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
